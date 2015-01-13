b8aeca326ce963ccf372ddd5f1b0d9856cc16a8c
James Pelster <jpmac26@gmail.com>
2015-01-12 16:23:41 -0800

    Delete Form1.vb

--- Form1.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Form1
-
-    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
-
-    End Sub
-End Class
