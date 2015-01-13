1fe8fdec631a9424146d93b97b28463685098754
James Pelster <jpmac26@gmail.com>
2015-01-12 16:22:24 -0800

    Add hotkey support, optimize code functionality
    The hotkeys are the same as the original Mario 64 Movie Maker: Control-1 to freeze, Control-2 to unfreeze, and Control-3 to toggle Change Camera Type. In the future, these hotkeys will be customizable.

--- M64MM2/Form1.vb
+++ M64MM2/Form1.vb
@@ -1,16 +1,11 @@
-﻿Public Class Form1
-    'My base address: 42D90000
+Imports System.Windows.Input
 
-    '430CC848 - Me
-    '5293C848 - MatthewGU4
-
-    Public ChangeCamera As Boolean = False
-    Public Base As UInteger
-    Public AddressToModify As UInteger
-    Private testStuck As Long
+Public Class Form1
+    Private ChangeCamera As Boolean = False
+    Private Base As Long
+	Private Key3WasUp As Boolean = True
 
     Public Sub New()
-
         ' This call is required by the designer.
         InitializeComponent()
 
@@ -22,49 +17,90 @@
     End Sub
 
     Private Sub GetBase()
+        ' Get the base RAM address of the emulated memory block by searching for the constant value of SM64's first RAM address
         Base = GetBaseAddress("Project64")
         If Base > 0 Then
             MessageBox.Show("The base address is: " & Hex(Base))
             Label1.Text = "The base address is: " & Hex(Base)
-            AddressToModify = (Base + &H33C848)
+            'AddressToModify = (Base + &H33C848)
         Else
             Label1.Text = "Project64 isn't open!"
         End If
     End Sub
+	
+	Private Sub Freeze()
+		ChangeCamera = False
+        WriteInteger("Project64", Base + &H33C848, &H80000000)
+	End Sub
+	
+	Private Sub Unfreeze()
+		ChangeCamera = False
+        WriteInteger("Project64", Base + &H33C848, 0)
+	End Sub
+	
+	Private Sub ChangeCameraType()
+		ChangeCamera = Not ChangeCamera
+		If ChangeCamera = True Then
+			b_ChangeCameraType.Text = "Go to new area"
+		Else
+			b_ChangeCameraType.Text = "Change Camera Type"
+		End If
+	End Sub
 
     Private Sub b_Freeze_Click(sender As Object, e As EventArgs) Handles b_Freeze.Click
-        ChangeCamera = False
-        WriteInteger("Project64", Base + &H33C848, &H80000000)
+        Freeze()
     End Sub
 
     Private Sub b_Unfreeze_Click(sender As Object, e As EventArgs) Handles b_Unfreeze.Click
-        ChangeCamera = False
-        WriteInteger("Project64", Base + &H33C848, 0)
-        'BackgroundWorker1.WorkerSupportsCancellation = True
-        'BackgroundWorker1.CancelAsync()
+        Unfreeze()
+    End Sub
+	
+	Private Sub b_ChangeCameraType_Click(sender As Object, e As EventArgs) Handles b_ChangeCameraType.Click
+        ChangeCameraType()
     End Sub
 
     Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
-        If Base <= 0 Then
+        ' Check if a valid base address was found. If not, stop the BackgroundWorker
+		If Base <= 0 Then
             MessageBox.Show("Project64 isn't open!")
             Exit Sub
-        End If
+        End If	
+		
+		' Main program update loop
         Do While Base > 0
-            testStuck = ReadLong("Project64", Base + &H33C848)
-            If testStuck >= &HA2000000L Then
+			' Handle key input (for hotkeys, etc.)
+			HandleInput()
+			
+			' Sometimes exiting first-person while the camera is frozen will result in a glitched state where Mario is stuck in first-person.
+			' This checks to see if this has happened, and forcibly fixes the camera if needed.
+            If ReadLong("Project64", Base + &H33C848) >= &HA2000000L Then
                 WriteInteger("Project64", Base + &H33C848, &H80000000)
             End If
-            Do
-                If ChangeCamera = False Then
-                    Exit Do
-                End If
-                WriteInteger("Project64", Base + &H33C848L, &H80000000)
+			
+			' If we are changing camera modes, repeatedly force the camera into frozen mode.
+            Do While ChangingCamera = True
+                WriteInteger("Project64", Base + &H33C848, &H80000000)
             Loop
         Loop
     End Sub
-
-    Private Sub ChangeCameraType_Click(sender As Object, e As EventArgs) Handles ChangeCameraType.Click
-        ChangeCamera = True
-        'WriteInteger("Project64", Base + &H8033C848, &H80000000)
-    End Sub
+	
+	Private Sub HandleInput()
+		If (Keyboard.GetKeyStates(Key.LeftControl) And KeyStates.Down) Or (Keyboard.GetKeyStates(Key.RightControl) And KeyStates.Down) Then
+			If (Keyboard.GetKeyStates(Key.D1) And KeyStates.Down) Then ' Pressing Control-1
+				Freeze()
+			ElseIf (Keyboard.GetKeyStates(Key.D2) And KeyStates.Down) Then ' Pressing Control-2
+				Unfreeze()
+			ElseIf (Keyboard.GetKeyStates(Key.D3) And KeyStates.Down) And Key3WasUp = True Then ' Pressing Control-3
+				' Because this switches the CameraChanged variable between true and false, we don't want it spamming if the user holds down the "3" key for a moment.
+				Key3WasUp = False
+				ChangeCameraType()
+			End If
+		End If
+		
+		If (Keyboard.GetKeyStates(Key.D3) And KeyStates.Up) Then
+			Key3WasUp = True
+		Else
+			Key3WasUp = False
+		End If
+	End Sub
 End Class
