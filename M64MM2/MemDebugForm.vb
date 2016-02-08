b10bf87bf1ecde0f8bc6713d8c91f68f32e12718
James Pelster <jpmac26@gmail.com>
2016-02-08 12:04:16 -0700

    More work on mem i/o debug, cleanup unused DMA i/o functions
    Still not finished, but the first set of read/write boxes sort of work

--- M64MM2/MemDebugForm.vb
+++ M64MM2/MemDebugForm.vb
@@ -1,17 +1,100 @@
-﻿Public Class MemDebugForm
+﻿Imports System.Globalization
 
-	Private ReadOnly Property WeGotABadassOverHere As Boolean
-		Get
-			Return CB_Accept.Checked
-		End Get
-	End Property
+Public Class MemDebugForm
 
-	Private Sub CB_Accept_CheckedChanged(sender As Object, e As EventArgs) Handles CB_Accept.CheckedChanged
-		If CB_Accept.Checked = True Then
-			GB_DebugControls.Enabled = True
-		Else
-			GB_DebugControls.Enabled = False
-		End If
-	End Sub
+    Private Address1 As UInteger
+    Private Address2 As UInteger
+    Private Address3 As UInteger
+    Private Address4 As UInteger
+    Private Value1 As UInteger
+    Private Value2 As UInteger
+    Private Value3 As UInteger
+    Private Value4 As UInteger
 
+    Private ReadOnly Property WeGotABadassOverHere As Boolean
+        Get
+            Return (CB_Accept.Checked And MainForm.EmuOpen And MainForm.Base > 0)
+        End Get
+    End Property
+
+    Private Sub CB_Accept_CheckedChanged(sender As Object, e As EventArgs) Handles CB_Accept.CheckedChanged
+        If WeGotABadassOverHere Then
+            GB_DebugControls.Enabled = True
+        Else
+            GB_DebugControls.Enabled = False
+        End If
+    End Sub
+
+    Private Sub IN_Address1_ValidateHex(sender As Object, Optional e As EventArgs = Nothing, Optional key As KeyEventArgs = Nothing) Handles IN_Address1.Leave, IN_Address1.KeyDown
+        If IN_Address1.Text IsNot Nothing AndAlso IN_Address1.Text <> "" Then
+            If (key IsNot Nothing AndAlso key.KeyCode <> Keys.Return) Or (key Is Nothing And e IsNot Nothing) Then
+                If IN_Address1.Text.StartsWith("0x") Or (IN_Address1.Text.StartsWith("80") And IN_Address1.Text.Length = 8) Then
+                    IN_Address1.Text = IN_Address1.Text.Substring(2)
+                End If
+
+                Try
+                    Address1 = UInteger.Parse(IN_Address1.Text, NumberStyles.HexNumber)
+                Catch ex As Exception
+                    MsgBox("The value you have entered is not a valid Hexadecimal address." & vbCrLf & ex.Message)
+                    'IN_Address1.Text = ""
+                End Try
+            End If
+        End If
+    End Sub
+
+    Private Sub IN_Value1_ValidateHex(sender As Object, Optional e As EventArgs = Nothing, Optional key As KeyEventArgs = Nothing) Handles IN_Value1.Leave, IN_Value1.KeyDown
+        If IN_Value1.Text IsNot Nothing AndAlso IN_Value1.Text <> "" Then
+            If (key IsNot Nothing AndAlso key.KeyCode <> Keys.Return) Or (key Is Nothing And e IsNot Nothing) Then
+                If IN_Value1.Text.StartsWith("0x") Then
+                    IN_Value1.Text = IN_Value1.Text.Substring(2)
+                End If
+
+                Try
+                    Value1 = UInteger.Parse(IN_Value1.Text, NumberStyles.HexNumber)
+                Catch ex As Exception
+                    MsgBox("The number you have entered is not a valid Hexadecimal value." & vbCrLf & ex.Message)
+                    'IN_Value1.Text = ""
+                End Try
+            End If
+        End If
+    End Sub
+
+    Private Sub B_Read1_Click(sender As Object, e As EventArgs) Handles B_Read1.Click
+        If WeGotABadassOverHere Then
+            Value1 = ReadUInteger("Project64", MainForm.Base + Address1)
+            IN_Value1.Text = Hex(Value1)
+        End If
+    End Sub
+
+    Private Sub B_Write1_Click(sender As Object, e As EventArgs) Handles B_Write1.Click
+        If WeGotABadassOverHere Then
+            If Address1 <= 4 Then
+                Dim WarningText As String = "You are attempting to write to the first four bytes of Super Mario 64's memory, " &
+                          "which contains the value this program uses to find the game's base address. " &
+                          "If you click ""Yes"", you will need to reset the emulator before this program will be able " &
+                          "to find Super Mario 64 again. Are you sure you want to do this?"
+                If MessageBox.Show(WarningText, "Are you sure?", MessageBoxButtons.YesNo) = DialogResult.Yes Then
+                    WriteUInteger("Project64", MainForm.Base + Address1, Value1)
+                End If
+            Else
+                WriteUInteger("Project64", MainForm.Base + Address1, Value1)
+            End If
+        End If
+    End Sub
+
+    Private Sub IN_Value1_ValidateHex(sender As Object, e As KeyEventArgs)
+
+    End Sub
+
+    Private Sub IN_Value1_ValidateHex(sender As Object, e As EventArgs) Handles IN_Value1.Leave, IN_Value1.KeyDown
+
+    End Sub
+
+    Private Sub IN_Address1_ValidateHex(sender As Object, e As KeyEventArgs)
+
+    End Sub
+
+    Private Sub IN_Address1_ValidateHex(sender As Object, e As EventArgs) Handles IN_Address1.Leave, IN_Address1.KeyDown
+
+    End Sub
 End Class
--- M64MM2/ReadWritingMemory.vb
+++ M64MM2/ReadWritingMemory.vb
@@ -1,252 +1,176 @@
 ﻿Module ReadWritingMemory
-	Private Declare Function OpenProcess Lib "kernel32" (ByVal dwDesiredAccess As Integer, ByVal bInheritHandle As Integer, ByVal dwProcessId As Integer) As Integer
+    Private Declare Function OpenProcess Lib "kernel32" (ByVal dwDesiredAccess As Integer, ByVal bInheritHandle As Integer, ByVal dwProcessId As Integer) As Integer
 
-	Private Declare Function WriteProcessMemory1 Lib "kernel32" Alias "WriteProcessMemory" (ByVal hProcess As Integer, ByVal lpBaseAddress As Integer, ByRef lpBuffer As Integer, ByVal nSize As Integer, ByRef lpNumberOfBytesWritten As Integer) As Integer
-	Private Declare Function WriteProcessMemory2 Lib "kernel32" Alias "WriteProcessMemory" (ByVal hProcess As Integer, ByVal lpBaseAddress As Integer, ByRef lpBuffer As Single, ByVal nSize As Integer, ByRef lpNumberOfBytesWritten As Integer) As Single
-	Private Declare Function WriteProcessMemory3 Lib "kernel32" Alias "WriteProcessMemory" (ByVal hProcess As Integer, ByVal lpBaseAddress As Integer, ByRef lpBuffer As Long, ByVal nSize As Integer, ByRef lpNumberOfBytesWritten As Integer) As Long
+    Private Declare Function WriteProcessMemory1 Lib "kernel32" Alias "WriteProcessMemory" (ByVal hProcess As Integer, ByVal lpBaseAddress As UInteger, ByRef lpBuffer As Integer, ByVal nSize As Integer, ByRef lpNumberOfBytesWritten As Integer) As Integer
+    Private Declare Function WriteProcessMemory2 Lib "kernel32" Alias "WriteProcessMemory" (ByVal hProcess As Integer, ByVal lpBaseAddress As UInteger, ByRef lpBuffer As Single, ByVal nSize As Integer, ByRef lpNumberOfBytesWritten As Integer) As Single
+    Private Declare Function WriteProcessMemory3 Lib "kernel32" Alias "WriteProcessMemory" (ByVal hProcess As Integer, ByVal lpBaseAddress As UInteger, ByRef lpBuffer As Long, ByVal nSize As Integer, ByRef lpNumberOfBytesWritten As Integer) As Long
+    Private Declare Function WriteProcessMemory4 Lib "kernel32" Alias "WriteProcessMemory" (ByVal hProcess As Integer, ByVal lpBaseAddress As UInteger, ByRef lpBuffer As UInteger, ByVal nSize As Integer, ByRef lpNumberOfBytesWritten As Integer) As UInteger
 
-	Private Declare Function ReadProcessMemory1 Lib "kernel32" Alias "ReadProcessMemory" (ByVal hProcess As Integer, ByVal lpBaseAddress As Integer, ByRef lpBuffer As Integer, ByVal nSize As Integer, ByRef lpNumberOfBytesRead As Integer) As Integer
-	Private Declare Function ReadProcessMemory2 Lib "kernel32" Alias "ReadProcessMemory" (ByVal hProcess As Integer, ByVal lpBaseAddress As Integer, ByRef lpBuffer As Single, ByVal nSize As Integer, ByRef lpNumberOfBytesRead As Integer) As Single
-	Private Declare Function ReadProcessMemory3 Lib "kernel32" Alias "ReadProcessMemory" (ByVal hProcess As Integer, ByVal lpBaseAddress As Integer, ByRef lpBuffer As Long, ByVal nSize As Integer, ByRef lpNumberOfBytesRead As Integer) As Long
-	Private Declare Function ReadProcessMemory4 Lib "kernel32" Alias "ReadProcessMemory" (ByVal hProcess As Integer, ByVal lpBaseAddress As Integer, ByVal lpBuffer() As Byte, ByVal iSize As Integer, ByRef lpNumberOfBytesRead As Integer) As Boolean
-	Private Declare Function ReadProcessMemory5 Lib "kernel32" Alias "ReadProcessMemory" (ByVal hProcess As Integer, ByVal lpBaseAddress As Integer, ByVal lpBuffer As Byte, ByVal iSize As Integer, ByRef lpNumberOfBytesRead As Integer) As Byte
+    Private Declare Function ReadProcessMemory1 Lib "kernel32" Alias "ReadProcessMemory" (ByVal hProcess As Integer, ByVal lpBaseAddress As UInteger, ByRef lpBuffer As Integer, ByVal nSize As Integer, ByRef lpNumberOfBytesRead As Integer) As Integer
+    Private Declare Function ReadProcessMemory2 Lib "kernel32" Alias "ReadProcessMemory" (ByVal hProcess As Integer, ByVal lpBaseAddress As UInteger, ByRef lpBuffer As Single, ByVal nSize As Integer, ByRef lpNumberOfBytesRead As Integer) As Single
+    Private Declare Function ReadProcessMemory3 Lib "kernel32" Alias "ReadProcessMemory" (ByVal hProcess As Integer, ByVal lpBaseAddress As UInteger, ByRef lpBuffer As Long, ByVal nSize As Integer, ByRef lpNumberOfBytesRead As Integer) As Long
+    Private Declare Function ReadProcessMemory4 Lib "kernel32" Alias "ReadProcessMemory" (ByVal hProcess As Integer, ByVal lpBaseAddress As UInteger, ByVal lpBuffer() As Byte, ByVal iSize As Integer, ByRef lpNumberOfBytesRead As Integer) As Boolean
+    Private Declare Function ReadProcessMemory5 Lib "kernel32" Alias "ReadProcessMemory" (ByVal hProcess As Integer, ByVal lpBaseAddress As UInteger, ByVal lpBuffer As Byte, ByVal iSize As Integer, ByRef lpNumberOfBytesRead As Integer) As Byte
+    Private Declare Function ReadProcessMemory6 Lib "kernel32" Alias "ReadProcessMemory" (ByVal hProcess As Integer, ByVal lpBaseAddress As UInteger, ByRef lpBuffer As UInteger, ByVal nSize As Integer, ByRef lpNumberOfBytesRead As Integer) As UInteger
 
-	Const PROCESS_ALL_ACCESS = &H1F0FF
+    Const PROCESS_ALL_ACCESS = &H1F0FF
 
-	Public Function WriteDMAInteger(ByVal Process As String, ByVal Address As Integer, ByVal Offsets As Integer(), ByVal Value As Integer, ByVal Level As Integer, Optional ByVal nsize As Integer = 4) As Boolean
-		Try
-			Dim lvl As Integer = Address
-			For i As Integer = 1 To Level
-				lvl = ReadInteger(Process, lvl, nsize) + Offsets(i - 1)
-			Next
-			WriteInteger(Process, lvl, Value, nsize)
-			Return True
-		Catch ex As Exception
-			Return False
-		End Try
-	End Function
+    Public Sub WriteXBytes(ByVal ProcessName As String, ByVal Address As Long, ByVal Value As String)
+        Dim hProcess As IntPtr = GetEmuProcess("Project64")
+        If hProcess = Nothing Then Exit Sub
 
-	Public Function ReadDMAInteger(ByVal Process As String, ByVal Address As Integer, ByVal Offsets As Integer(), ByVal Level As Integer, Optional ByVal nsize As Integer = 4) As Integer
-		Try
-			Dim lvl As Integer = Address
-			For i As Integer = 1 To Level
-				lvl = ReadInteger(Process, lvl, nsize) + Offsets(i - 1)
-			Next
-			Dim vBuffer As Integer
-			vBuffer = ReadInteger(Process, lvl, nsize)
-			Return vBuffer
-		Catch ex As Exception
-			Return Nothing
-		End Try
-	End Function
+        Dim C As Integer
+        Dim B As Integer
+        Dim D As Integer
+        Dim V As Byte
 
-	Public Function WriteDMAFloat(ByVal Process As String, ByVal Address As Integer, ByVal Offsets As Integer(), ByVal Value As Single, ByVal Level As Integer, Optional ByVal nsize As Integer = 4) As Boolean
-		Try
-			Dim lvl As Integer = Address
-			For i As Integer = 1 To Level
-				lvl = ReadFloat(Process, lvl, nsize) + Offsets(i - 1)
-			Next
-			WriteFloat(Process, lvl, Value, nsize)
-			Return True
-		Catch ex As Exception
-			Return False
-		End Try
-	End Function
+        B = 0
+        D = 1
+        For C = 1 To Math.Round((Len(Value) / 2))
+            V = Val("&H" & Mid$(Value, D, 2))
+            Call WriteProcessMemory1(hProcess, Address + B, V, 1, 0&)
+            B = B + 1
+            D = D + 2
+        Next C
 
-	Public Function ReadDMAFloat(ByVal Process As String, ByVal Address As Integer, ByVal Offsets As Integer(), ByVal Level As Integer, Optional ByVal nsize As Integer = 4) As Single
-		Try
-			Dim lvl As Integer = Address
-			For i As Integer = 1 To Level
-				lvl = ReadFloat(Process, lvl, nsize) + Offsets(i - 1)
-			Next
-			Dim vBuffer As Single
-			vBuffer = ReadFloat(Process, lvl, nsize)
-			Return vBuffer
-		Catch ex As Exception
-			Return Nothing
-		End Try
-	End Function
+    End Sub
 
-	Public Function WriteDMALong(ByVal Process As String, ByVal Address As Integer, ByVal Offsets As Integer(), ByVal Value As Long, ByVal Level As Integer, Optional ByVal nsize As Integer = 4) As Boolean
-		Try
-			Dim lvl As Integer = Address
-			For i As Integer = 1 To Level
-				lvl = ReadLong(Process, lvl, nsize) + Offsets(i - 1)
-			Next
-			WriteLong(Process, lvl, Value, nsize)
-			Return True
-		Catch ex As Exception
-			Return False
-		End Try
-	End Function
+    Public Sub WriteInteger(ByVal ProcessName As String, ByVal Address As UInteger, ByVal Value As Integer, Optional ByVal nsize As Integer = 4)
+        Dim hProcess As IntPtr = GetEmuProcess("Project64")
+        If hProcess = Nothing Then Exit Sub
 
-	Public Function ReadDMALong(ByVal Process As String, ByVal Address As Integer, ByVal Offsets As Integer(), ByVal Level As Integer, Optional ByVal nsize As Integer = 4) As Long
-		Try
-			Dim lvl As Integer = Address
-			For i As Integer = 1 To Level
-				lvl = ReadLong(Process, lvl, nsize) + Offsets(i - 1)
-			Next
-			Dim vBuffer As Long
-			vBuffer = ReadLong(Process, lvl, nsize)
-			Return vBuffer
-		Catch ex As Exception
-			Return Nothing
-		End Try
-	End Function
+        Dim hAddress, vBuffer As Integer
+        hAddress = Address
+        vBuffer = Value
+        WriteProcessMemory1(hProcess, hAddress, CInt(vBuffer), nsize, 0)
+    End Sub
 
-	Public Sub WriteNOPs(ByVal ProcessName As String, ByVal Address As Long, ByVal NOPNum As Integer)
-		Dim C As Integer
-		Dim B As Integer
-		Dim hProcess As IntPtr = GetEmuProcess("Project64")
-		If hProcess = Nothing Then Exit Sub
+    Public Sub WriteUInteger(ByVal ProcessName As String, ByVal Address As UInteger, ByVal Value As UInteger, Optional ByVal nsize As Integer = 4)
+        Dim hProcess As IntPtr = GetEmuProcess("Project64")
+        If hProcess = Nothing Then Exit Sub
 
-		B = 0
-		For C = 1 To NOPNum
-			Call WriteProcessMemory1(hProcess, Address + B, &H90, 1, 0&)
-			B = B + 1
-		Next C
-	End Sub
+        Dim hAddress, vBuffer As UInteger
+        hAddress = Address
+        vBuffer = Value
+        WriteProcessMemory4(hProcess, hAddress, vBuffer, nsize, 0)
+    End Sub
 
-	Public Sub WriteXBytes(ByVal ProcessName As String, ByVal Address As Long, ByVal Value As String)
-		Dim hProcess As IntPtr = GetEmuProcess("Project64")
-		If hProcess = Nothing Then Exit Sub
+    Public Sub WriteFloat(ByVal ProcessName As String, ByVal Address As UInteger, ByVal Value As Single, Optional ByVal nsize As Integer = 4)
+        Dim hProcess As IntPtr = GetEmuProcess("Project64")
+        If hProcess = Nothing Then Exit Sub
 
-		Dim C As Integer
-		Dim B As Integer
-		Dim D As Integer
-		Dim V As Byte
+        Dim hAddress As Integer
+        Dim vBuffer As Single
 
-		B = 0
-		D = 1
-		For C = 1 To Math.Round((Len(Value) / 2))
-			V = Val("&H" & Mid$(Value, D, 2))
-			Call WriteProcessMemory1(hProcess, Address + B, V, 1, 0&)
-			B = B + 1
-			D = D + 2
-		Next C
+        hAddress = Address
+        vBuffer = Value
+        WriteProcessMemory2(hProcess, hAddress, vBuffer, nsize, 0)
+    End Sub
 
-	End Sub
+    Public Sub WriteLong(ByVal ProcessName As String, ByVal Address As UInteger, ByVal Value As Long, Optional ByVal nsize As Integer = 4)
+        Dim hProcess As IntPtr = GetEmuProcess("Project64")
+        If hProcess = Nothing Then Exit Sub
 
-	Public Sub WriteInteger(ByVal ProcessName As String, ByVal Address As Integer, ByVal Value As Integer, Optional ByVal nsize As Integer = 4)
-		Dim hProcess As IntPtr = GetEmuProcess("Project64")
-		If hProcess = Nothing Then Exit Sub
+        Dim hAddress As Integer
+        Dim vBuffer As Long
 
-		Dim hAddress, vBuffer As Integer
-		hAddress = Address
-		vBuffer = Value
-		WriteProcessMemory1(hProcess, hAddress, CInt(vBuffer), nsize, 0)
-	End Sub
+        hAddress = Address
+        vBuffer = Value
+        WriteProcessMemory3(hProcess, hAddress, vBuffer, nsize, 0)
+    End Sub
 
-	Public Sub WriteFloat(ByVal ProcessName As String, ByVal Address As Integer, ByVal Value As Single, Optional ByVal nsize As Integer = 4)
-		Dim hProcess As IntPtr = GetEmuProcess("Project64")
-		If hProcess = Nothing Then Exit Sub
+    Public Function ReadByte(ByVal ProcessName As String, ByVal Address As UInteger, Optional ByVal nsize As Integer = 1) As Byte()
+        Dim hProcess As IntPtr = GetEmuProcess("Project64")
+        If hProcess = Nothing Then Return Nothing
 
-		Dim hAddress As Integer
-		Dim vBuffer As Single
+        Dim hAddress As Integer
+        Dim vBuffer(0) As Byte
 
-		hAddress = Address
-		vBuffer = Value
-		WriteProcessMemory2(hProcess, hAddress, vBuffer, nsize, 0)
-	End Sub
+        hAddress = Address
+        ReadProcessMemory4(hProcess, hAddress, vBuffer, nsize, 0)
+        Return vBuffer
+    End Function
 
-	Public Sub WriteLong(ByVal ProcessName As String, ByVal Address As Integer, ByVal Value As Long, Optional ByVal nsize As Integer = 4)
-		Dim hProcess As IntPtr = GetEmuProcess("Project64")
-		If hProcess = Nothing Then Exit Sub
+    Public Function ReadInteger(ByVal ProcessName As String, ByVal Address As UInteger, Optional ByVal nsize As Integer = 4) As Integer
+        Dim hProcess As IntPtr = GetEmuProcess("Project64")
+        If hProcess = Nothing Then Return Nothing
 
-		Dim hAddress As Integer
-		Dim vBuffer As Long
+        Dim hAddress, vBuffer As Integer
+        hAddress = Address
+        ReadProcessMemory1(hProcess, hAddress, vBuffer, nsize, 0)
+        Return vBuffer
+    End Function
 
-		hAddress = Address
-		vBuffer = Value
-		WriteProcessMemory3(hProcess, hAddress, vBuffer, nsize, 0)
-	End Sub
+    Public Function ReadUInteger(ByVal ProcessName As String, ByVal Address As UInteger, Optional ByVal nsize As Integer = 4) As UInteger
+        Dim hProcess As IntPtr = GetEmuProcess("Project64")
+        If hProcess = Nothing Then Return Nothing
 
-	Public Function ReadByte(ByVal ProcessName As String, ByVal Address As Integer, Optional ByVal nsize As Integer = 1) As Byte()
-		Dim hProcess As IntPtr = GetEmuProcess("Project64")
-		If hProcess = Nothing Then Return Nothing
+        Dim hAddress, vBuffer As UInteger
+        hAddress = Address
+        ReadProcessMemory6(hProcess, hAddress, vBuffer, nsize, 0)
+        Return vBuffer
+    End Function
 
-		Dim hAddress As Integer
-		Dim vBuffer(0) As Byte
+    Public Function ReadFloat(ByVal ProcessName As String, ByVal Address As UInteger, Optional ByVal nsize As Integer = 4) As Single
+        Dim hProcess As IntPtr = GetEmuProcess("Project64")
+        If hProcess = Nothing Then Return Nothing
 
-		hAddress = Address
-		ReadProcessMemory4(hProcess, hAddress, vBuffer, nsize, 0)
-		Return vBuffer
-	End Function
+        Dim hAddress As Integer
+        Dim vBuffer As Single
 
-	Public Function ReadInteger(ByVal ProcessName As String, ByVal Address As Integer, Optional ByVal nsize As Integer = 4) As Integer
-		Dim hProcess As IntPtr = GetEmuProcess("Project64")
-		If hProcess = Nothing Then Return Nothing
+        hAddress = Address
+        ReadProcessMemory2(hProcess, hAddress, vBuffer, nsize, 0)
+        Return vBuffer
+    End Function
 
-		Dim hAddress, vBuffer As Integer
-		hAddress = Address
-		ReadProcessMemory1(hProcess, hAddress, vBuffer, nsize, 0)
-		Return vBuffer
-	End Function
+    Public Function ReadLong(ByVal ProcessName As String, ByVal Address As UInteger, Optional ByVal nsize As Integer = 4) As Long
+        Dim hProcess As IntPtr = GetEmuProcess("Project64")
+        If hProcess = Nothing Then Return Nothing
 
-	Public Function ReadFloat(ByVal ProcessName As String, ByVal Address As Integer, Optional ByVal nsize As Integer = 4) As Single
-		Dim hProcess As IntPtr = GetEmuProcess("Project64")
-		If hProcess = Nothing Then Return Nothing
+        Dim hAddress As Integer
+        Dim vBuffer As Long
 
-		Dim hAddress As Integer
-		Dim vBuffer As Single
+        hAddress = Address
+        ReadProcessMemory3(hProcess, hAddress, vBuffer, nsize, 0)
+        Return vBuffer
+    End Function
 
-		hAddress = Address
-		ReadProcessMemory2(hProcess, hAddress, vBuffer, nsize, 0)
-		Return vBuffer
-	End Function
+    Public Function GetEmuProcess(ProcessName As String) As IntPtr
+        If ProcessName.EndsWith(".exe") Then
+            ProcessName = ProcessName.Replace(".exe", "")
+        End If
+        Dim MyP As Process() = Process.GetProcessesByName(ProcessName)
+        If MyP.Length = 0 Then
+            Return Nothing
+        End If
+        Dim hProcess As IntPtr = OpenProcess(PROCESS_ALL_ACCESS, 0, MyP(0).Id)
+        If hProcess = IntPtr.Zero Then
+            MessageBox.Show("Failed to open " & ProcessName & "!")
+            Return Nothing
+        End If
+        Return hProcess
+    End Function
 
-	Public Function ReadLong(ByVal ProcessName As String, ByVal Address As Integer, Optional ByVal nsize As Integer = 4) As Long
-		Dim hProcess As IntPtr = GetEmuProcess("Project64")
-		If hProcess = Nothing Then Return Nothing
+    Public Function GetBaseAddress(ByVal ProcessName As String, Optional scanStep As Integer = &H10000, Optional ByVal nsize As Integer = 4) As Integer
 
-		Dim hAddress As Integer
-		Dim vBuffer As Long
+        Dim hProcess As IntPtr = GetEmuProcess("Project64")
+        If hProcess = Nothing Then Return 0
 
-		hAddress = Address
-		ReadProcessMemory3(hProcess, hAddress, vBuffer, nsize, 0)
-		Return vBuffer
-	End Function
+        Dim vBuffer As Integer
+        Dim startPoint As Integer = &H10000000
 
-	Public Function GetEmuProcess(ProcessName As String, Optional silent As Boolean = True) As IntPtr
-		If ProcessName.EndsWith(".exe") Then
-			ProcessName = ProcessName.Replace(".exe", "")
-		End If
-		Dim MyP As Process() = Process.GetProcessesByName(ProcessName)
-		If MyP.Length = 0 Then
-			If silent = False Then
-				MessageBox.Show(ProcessName & " isn't open!")
-			End If
-			Return Nothing
-		End If
-		Dim hProcess As IntPtr = OpenProcess(PROCESS_ALL_ACCESS, 0, MyP(0).Id)
-		If hProcess = IntPtr.Zero Then
-			MessageBox.Show("Failed to open " & ProcessName & "!")
-			Return Nothing
-		End If
-		Return hProcess
-	End Function
+        'If scanStep < &H1000 And scanStep >= &H100 Then
+        '    startPoint = &H20000000
+        'ElseIf scanStep < &H100 Then
+        '    startPoint = &H15000000
+        'End If
 
-	Public Function GetBaseAddress(ByVal ProcessName As String, silent As Boolean, Optional scanStep As Integer = &H10000, Optional ByVal nsize As Integer = 4) As Integer
+        For x = startPoint To &H70000000 Step scanStep
+            ReadProcessMemory1(hProcess, x, vBuffer, nsize, 0)
 
-		Dim hProcess As IntPtr = GetEmuProcess("Project64", silent)
-		If hProcess = Nothing Then Return 0
+            If vBuffer = &H3C1A8032 Then Return x
+        Next
 
-		Dim vBuffer As Integer
-		Dim startPoint As Integer = &H10000000
-
-		'If scanStep < &H1000 And scanStep >= &H100 Then
-		'    startPoint = &H20000000
-		'ElseIf scanStep < &H100 Then
-		'    startPoint = &H15000000
-		'End If
-
-		For x = startPoint To &H70000000 Step scanStep
-			ReadProcessMemory1(hProcess, x, vBuffer, nsize, 0)
-
-			If vBuffer = &H3C1A8032 Then Return x
-		Next
-
-		Return 0
-	End Function
-
+        Return 0
+    End Function
 End Module
