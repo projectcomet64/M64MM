d5f3e25b39648fda4bc7656cdd15e06beea48368
GlitchyPSIX <supermglitchy3@gmail.com>
2019-02-24 10:41:07 -0400

    Y.E.N. - Start moving Color Code API to Utils

--- M64MM.Utils/Utils.cs
+++ M64MM.Utils/Utils.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
+using System.Drawing;
 using System.Collections;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
@@ -17,6 +18,21 @@ namespace M64MM
             VANILLA,
             EMPTY,
             MODDED
+        }
+        public enum VanillaModelColor
+        {
+            PantsShade,
+            PantsMain,
+            HatShade,
+            HatMain,
+            GloveShade,
+            GloveMain,
+            ShoeShade,
+            ShoeMain,
+            SkinShade,
+            SkinMain,
+            HairShade,
+            HairMain
         }
         static Process emuProcess;
         static IntPtr emuProcessHandle;
@@ -163,6 +179,58 @@ namespace M64MM
             BaseAddress = 0;
         }
 
+        public static void WriteColor(VanillaModelColor ModelPart, Color color_)
+        {
+            long writeToAddress = 0;
+            byte[] colors = new byte[4];
+                switch (ModelPart)
+                {
+                    case VanillaModelColor.PantsShade:
+                        writeToAddress = 0x07EC20;
+                        break;
+                    case VanillaModelColor.PantsMain:
+                        writeToAddress = 0x07EC28;
+                        break;
+                    case VanillaModelColor.HatShade:
+                        writeToAddress = 0x07EC38;
+                        break;
+                    case VanillaModelColor.HatMain:
+                        writeToAddress = 0x07EC40;
+                        break;
+                    case VanillaModelColor.GloveShade:
+                        writeToAddress = 0x07EC50;
+                        break;
+                    case VanillaModelColor.GloveMain:
+                        writeToAddress = 0x07EC58;
+                        break;
+                    case VanillaModelColor.ShoeShade:
+                        writeToAddress = 0x07EC68;
+                        break;
+                    case VanillaModelColor.ShoeMain:
+                        writeToAddress = 0x07EC70;
+                        break;
+                    case VanillaModelColor.SkinShade:
+                        writeToAddress = 0x07EC80;
+                        break;
+                    case VanillaModelColor.SkinMain:
+                        writeToAddress = 0x07EC88;
+                        break;
+                    case VanillaModelColor.HairShade:
+                        writeToAddress = 0x07EC98;
+                        break;
+                    case VanillaModelColor.HairMain:
+                        writeToAddress = 0x07ECA0;
+                        break;
+                }
+            colors[0] = color_.R;
+            colors[1] = color_.G;
+            colors[2] = color_.B;
+            colors[3] = 0x0;
+            if (writeToAddress > 0)
+            {
+                WriteBytes(BaseAddress + writeToAddress, SwapEndian(colors, 4));
+            }
+        }
 
         public static ModelStatus ValidateModel()
         {
@@ -231,14 +299,16 @@ namespace M64MM
         }
     }
 
+
+
     public interface IModule
     {
         void Initialize();
         void Update();
         void Reset();
         void Close(System.ComponentModel.CancelEventArgs e);
-        string SafeName { get; set; }
-        string Description { get; set; }
+        string SafeName { get; }
+        string Description { get; }
         ICollection GetCommands();
     }
 
